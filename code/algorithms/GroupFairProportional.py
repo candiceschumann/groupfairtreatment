394c896b569894a954a53fe4a86cef0b8ad124e3
candices <candices@umd.edu>
2019-01-15 10:44:21 -0500

    added group fair proportional

---
+++ code/algorithms/GroupFairProportional.py
@@ -0,0 +1,35 @@
+from TopIntervalContextualBandit import *
+
+class GroupFairProportionalBandit(TopIntervalContextualBandit):
+
+	def __init__(self, num_arms, context_size, delta, T, groups):
+		super().__init__(num_arms, context_size, delta, T)
+		self.groups = groups
+
+	'''Return an arm given the context and timestep.'''
+	def pick_arm(self,context,t):
+		# Randomly picking an arm maintains proportions
+		if np.random.random() < self.eta(t):
+			return np.random.randint(0,self.num_arms)
+		else:
+			l_u = self.lower_uppers(context)
+			# Pick a group according to proportions
+			rnd = np.random.random()
+			tot = 0
+			for gp in groups:
+				tot += len(groups[gp])
+				if rnd < (tot * 1.0) / self.num_arms:
+					group = gp
+					break
+			# In that group choose the arm with the 
+			# highest upper confidence.
+			arm = groups[group][0]
+			for i in groups[group]:
+				if l_u[i].upper > l_u[arm].upper:
+					arm = i
+			return arm
+
+groups = {'Female': [1], 'Male': [0,2]}
+bandit = GroupFairProportionalBandit(3,4,0.5,10,groups)
+context = [np.random.random(4) for _ in range(3)]
+print(bandit.pick_arm(context,100))
