34a08c56f4f6ae1e253611bb4c661d57078f9b29
candices <candices@umd.edu>
2019-02-06 13:09:55 -0500

    changing experiments slightly

--- code/run_experiments.py
+++ code/run_experiments.py
@@ -23,3 +23,18 @@ if args.exp == "GenderRatiosUniform":
 			os.makedirs(os.path.dirname(filename))
 		experiment = Experiment(arms, context, groups, algorithms, deltas, Ts, "uniform", filename=filename)
 		experiment.run_x_experiments(runs)
+elif args.exp == "GenderRatiosUniformSmaller":
+	arms = 10
+	runs = 10
+	context = 2
+	ratios = [1,2,3,4,5]
+	deltas = [0.5,0.4,0.3,0.2,0.1]
+	Ts = [10,20,30,40,50,60,70,80,90,100,250,500,750,1000,1500,2000]
+	algorithms = ["TopInterval", "IntervalChaining", "Random", "GroupFairParity", "GroupFairParityInterval", "GroupFairProportional", "GroupFairProportionalInterval"]
+	for ratio in ratios:
+		groups = {"g1": [i for i in range(0,ratio)], "g2": [i for i in range(ratio,arms)]}
+		filename = "../experiments/%s/%s" % (args.exp,ratio)
+		if not os.path.exists(os.path.dirname(filename)):
+			os.makedirs(os.path.dirname(filename))
+		experiment = Experiment(arms, context, groups, algorithms, deltas, Ts, "uniform", filename=filename)
+		experiment.run_x_experiments(runs)
