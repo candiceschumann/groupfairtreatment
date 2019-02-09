09487acb6a1b35136a9002d315d4739c5e9bcaa0
candices <candices@umd.edu>
2019-02-08 20:14:37 -0500

    expanding smaller experiments

--- code/run_experiments.py
+++ code/run_experiments.py
@@ -26,16 +26,17 @@ if args.exp == "GenderRatiosUniform":
 		experiment.run_x_experiments(runs)
 elif args.exp == "GenderRatiosUniformSmaller":
 	arms = 10
-	runs = 10
-	context = 2
+	runs = 20
+	contexts = [2,3,4,5,6,7,8,9,10]
 	ratios = [1,2,3,4,5]
 	deltas = [0.5,0.4,0.3,0.2,0.1]
 	Ts = [10,20,30,40,50,60,70,80,90,100,250,500,750,1000,1500,2000]
 	algorithms = ["TopInterval", "IntervalChaining", "Random", "GroupFairParity", "GroupFairParityInterval", "GroupFairProportional", "GroupFairProportionalInterval"]
-	for ratio in ratios:
-		groups = {"g1": [i for i in range(0,ratio)], "g2": [i for i in range(ratio,arms)]}
-		filename = "../experiments/%s/%s_%s" % (args.exp,ratio,args.run_name)
-		if not os.path.exists(os.path.dirname(filename)):
-			os.makedirs(os.path.dirname(filename))
-		experiment = Experiment(arms, context, groups, algorithms, deltas, Ts, "uniform", filename=filename)
-		experiment.run_x_experiments(runs)
+	for context in contexts:
+		for ratio in ratios:
+			groups = {"g1": [i for i in range(0,ratio)], "g2": [i for i in range(ratio,arms)]}
+			filename = "../experiments/%s/ratio_%s_context_%s_%s" % (args.exp,ratio,context,args.run_name)
+			if not os.path.exists(os.path.dirname(filename)):
+				os.makedirs(os.path.dirname(filename))
+			experiment = Experiment(arms, context, groups, algorithms, deltas, Ts, "uniform", filename=filename)
+			experiment.run_x_experiments(runs)
