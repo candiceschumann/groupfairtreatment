82e57247ba6277325ed38770fcf6ddfe1d7f1b26
Willy Lang <krixly@Willys-MacBook-Pro.local>
2019-03-29 15:10:10 -0400

    added vary bound experiments

--- code/run_experiments.py
+++ code/run_experiments.py
@@ -84,10 +84,31 @@ elif args.exp == 'VaryNumGroups':
 		groups = {"g{}".format(i+1): group_indices[i] for i in range(num_groups)}
 		for c in cs:
 			hardness = {"g{}".format(i+1): (0, c) for i in range(num_groups)}
-			filename = "../experiments/%s/_context_%s_c_%s_%s" % (args.exp,context_size,c,args.run_name)
+			filename = "../experiments/%s/_context_%s_numgrp_%s_c_%s_%s" % (args.exp,context_size,num_groups,args.run_name)
 			if not os.path.exists(os.path.dirname(filename)):
 				os.makedirs(os.path.dirname(filename))
 			experiment = Experiment(arms, context_size, groups, algorithms, deltas, Ts, "uniform", filename=filename, cs=hardness)
 			experiment.run_x_experiments(runs)
+elif args.exp == 'VaryBounds':
+	arms = 10
+	deltas = [.1, .2, .3, .4, .5]
+	cs = [1, 2, 5, 10]
+	runs = 20
+	ratios = list(range(2, 6))
+	Ts = [2 * n * arms for n in range(1, 21)]
+	algorithms = ["TopInterval", "IntervalChaining", "Random", "GroupFairParity", "GroupFairParityInterval",
+	              "GroupFairProportional", "GroupFairProportionalInterval"]
+	context_size = 2
 
-
+	c_covered = [i/10 for i in range(4, 11)]
+	for percentage in c_covered:
+		for ratio in ratios:
+			groups = {"g1": [i for i in range(0, ratio)], "g2": [i for i in range(ratio, arms)]}
+			for c in cs:
+				hardness = {"g1": (0, percentage * c), "g2": (c - percentage * c, c)}
+				filename = "../experiments/%s/_context_%s_percentc_%s_c_%s_%s" % (args.exp, context_size, percentage, args.run_name)
+				if not os.path.exists(os.path.dirname(filename)):
+					os.makedirs(os.path.dirname(filename))
+				experiment = Experiment(arms, context_size, groups, algorithms, deltas, Ts, "uniform",
+				                        filename=filename, cs=hardness)
+				experiment.run_x_experiments(runs)
