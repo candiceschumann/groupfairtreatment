eee9163ab9a7bca3a0e63f4644f4c4d77d588f93
candices <candices@umd.edu>
2019-08-17 22:03:21 -0700

    error experiment

--- code/run_synth.py
+++ code/run_synth.py
@@ -96,5 +96,34 @@ if __name__ == "__main__":
                         cs=hardness,sensitive_group=sensitive_group,group_mean=error_mean, 
                         group_std=error_std)
             experiment.run_x_experiments(runs,seeds=[np.random.random_integers(100000000) for _ in range(runs)])
+    if args.exp == "error":
+        arms = 10
+        Ts = [1000]
+        context_size = 2
+        ratio = 5
+        groups = {"g1": [i for i in range(0, ratio)], "g2": [i for i in range(ratio, arms)]}
+        sensitive_group = {}
+        for i in range(0,ratio):
+            sensitive_group[i] = True
+        for i in range(ratio,arms):
+            sensitive_group[i] = False
+        c = 10
+        hardness = {"g1": (0, c), "g2": (0, c)}
+        delta = 0.1
+        error_means = [-i for i in range(10)]
+        error_std = 1
+        for error_mean in error_means:
+            filename = "../experiments/%s/context_%s_ratio_%s_c_%s_delta_%s_error_%s_arms_%s_run_%s" % (args.exp, 
+                        context_size, ratio, c, delta, error_mean, arms, args.run_name)
+            if not os.path.exists(os.path.dirname(filename)):
+                os.makedirs(os.path.dirname(filename))
+
+            experiment = Experiment(arms,context_size,groups,
+                        bandit_types=algorithms,deltas=[delta],
+                        Ts=Ts,arm_type="uniform",
+                        filename=filename,
+                        cs=hardness,sensitive_group=sensitive_group,group_mean=error_mean, 
+                        group_std=error_std)
+            experiment.run_x_experiments(runs,seeds=[np.random.random_integers(100000000) for _ in range(runs)])
 
 
