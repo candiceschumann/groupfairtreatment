e2bb69f49e8b6da0e59368166a2937de422da104
candices <candices@umd.edu>
2020-01-30 13:54:25 -0500

    added bash script

---
+++ code/run_synth.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for run_number in 1 2 3 4 5 6 7 8 9 10
+do
+    for experiment_type in T context_size c error ratio delt arms
+    do
+        echo $experiment_type
+        python3 run_synth.py $experiment_type $run_number
+    done
+done
