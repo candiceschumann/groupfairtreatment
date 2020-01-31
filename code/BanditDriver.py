I somehow added another bug - yay off by one errors

code/BanditDriver.py
@@ -1,5 +1,5 @@
 import numpy as np
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 class BanditDriver:
 
@@ -20,20 +20,26 @@ class BanditDriver:
 			raise ValueError('Running for %d steps is more than the %d allowed') % (self.t + t, self.bandit.get_T())
 		reward_sum = 0
 		real_reward_sum = 0
+		opt_reward_sum = 0
+		opt_real_reward_sum = 0
 		for i in range(t):
 
 			self.t += 1
 			# Get new context for this round from all of the arms.
 			contexts = [self.arms[arm].get_new_context() for arm in range(len(self.arms))]
 			true_rewards = [self.arms[arm].get_reward(contexts[arm]) for arm in range(len(self.arms))]
-			print (true_rewards)
+			# print (true_rewards)
 			unbiased_rewards = [self.arms[arm].get_real_reward(contexts[arm]) for arm in range(len(self.arms))]
-			print (unbiased_rewards)
+			# print (unbiased_rewards)
 			self.opt_arms.append(np.argmax(true_rewards))
 			self.opt_real_arms.append(np.argmax(unbiased_rewards))
 			self.opt_rewards.append(true_rewards[self.opt_arms[-1]])
+			opt_reward_sum += self.opt_rewards[-1]
 			self.opt_real_rewards.append(unbiased_rewards[self.opt_real_arms[-1]])
+			opt_real_reward_sum += self.opt_real_rewards[-1]
 			print("round " + str(i))
+			print("opt_real_rewards " + str(self.opt_real_rewards[-1]))
+			print("opt reward " + str(self.opt_rewards[-1]))
 			# Pick an arm to pull and pull it
 			arm = self.bandit.pick_arm(contexts, self.t)
 			try:
@@ -56,6 +62,13 @@ class BanditDriver:
 			self.bandit.update(arm, contexts[arm], reward)
 		print("reward sum " + str(reward_sum/500))
 		print("real reward sum " + str(real_reward_sum/500))
+		print(" opt reward_sum " + str(opt_reward_sum/500))
+		print(" opt real reward sum " + str(opt_real_reward_sum/500))
+		print(" average real regret " + str(np.mean(np.array(self.opt_real_rewards) - np.array(self.real_rewards))))
+		print( "average regret " + str(np.mean(np.array(self.opt_rewards) - np.array(self.rewards))))
+		plt.plot(np.array(self.opt_real_rewards) - np.array(self.real_rewards))
+		plt.plot(np.array(self.opt_rewards) - np.array(self.rewards))
+		# plt.show()
 
 
 	def complete_run(self):

code/algorithms/GroupFairTopInterval.py
@@ -91,8 +91,8 @@ class GroupFairTopIntervalBandit(TopIntervalContextualBandit):
 			group = 1
 		else:
 			group = 0
-		sens_confidence = self.group_confidence(0, context)
-		nonsens_confidence = self.group_confidence(1, context)
+		sens_confidence = self.group_confidence(1, context)
+		nonsens_confidence = self.group_confidence(0, context)
 		# if group:
 		if math.isinf(w) or Y is None:
 			return float('inf')
@@ -101,8 +101,10 @@ class GroupFairTopIntervalBandit(TopIntervalContextualBandit):
 		else:
 			if self.group_beta[0] is None or self.group_beta[1] is None:
 				return float('inf')
-			Y_sens = np.dot(self.group_beta[0].T, context)
-			Y_nonsens = np.dot(self.group_beta[1].T, context)
+			Y_sens = np.dot(self.group_beta[1].T, context)
+			Y_nonsens = np.dot(self.group_beta[0].T, context)
+			print(Y_sens)
+			print(Y_nonsens)
 			return (Y+w - Y_sens + sens_confidence + Y_nonsens + nonsens_confidence)[0]
 			# return (Y + w - np.dot(self.group_beta[group].T, context) + sens_confidence + \
 			#        np.dot(np.ones_like(self.group_beta[group]).T, context)*20)[0]

code/algorithms/MultiGroupFairTopInterval.py
@@ -49,7 +49,7 @@ class MultiGroupFairTopIntervalBandit(GroupFairTopIntervalBandit):
             return float('inf')
         else:
             Y_group = np.dot(self.group_beta[group].T, context)
-            return (Y+w + self.mu - Y_group + group_confidence)[0]
+            return (Y+w + (self.mu - Y_group + group_confidence)*0.9)[0]
 
 
 

code/run_synth.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 
     algorithms = ["TopInterval", "IntervalChaining", "GroupFairTopInterval", "MultiGroupFairTopInterval"]
     runs = 10
-    mu=30
+    mu=58
     seeds = [np.random.random_integers(100000000) for _ in range(runs)]
 
     if args.exp == 'debug':